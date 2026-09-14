ad8e40e657783a612df2817582679bc7adb27b44
Ala Jouili <ala.jouili@supcom.tn>
2026-09-14 20:23:25 +0100

    cd

--- terraform-s3-bucket/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_arn" {
-  description = "The ARN of the created S3 bucket."
-  value       = aws_s3_bucket.my_bucket.arn
-}
--- terraform-s3-bucket/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region  = "us-east-1"
-bucket_name = "my-devops-book-bucket-98765-claud"
