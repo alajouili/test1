9eb6b246a49f51dabed8de77a618e0eef798c8d9
Ala Jouili <ala.jouili@supcom.tn>
2026-09-17 17:06:52 +0100

    wiw

--- simple-flask-app/app.py
+++ simple-flask-app/app.py
@@ -1,10 +1,11 @@
-
 from flask import Flask
+import awsgi
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from inside a Docker Container!\n'
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context)
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,69 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_apprunner_service" "staging" {
-  service_name = "simple-flask-app-staging"
+resource "aws_iam_role" "lambda_role" {
+  name = "simple-flask-app-lambda-role"
 
-  source_configuration {
-    image_repository {
-      image_identifier      = var.ecr_image_identifier
-      image_repository_type = "ECR"
-    }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
 
-    auto_deployments_enabled = false
-  }
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_ecr_repository_policy" "lambda_access" {
+  repository = "simple-flask-app"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "LambdaECRImageAccess"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "staging" {
+  function_name = "simple-flask-app-staging"
+
+  package_type = "Image"
+
+  image_uri = var.ecr_image_identifier
+
+  role = aws_iam_role.lambda_role.arn
+
+  timeout     = 30
+  memory_size = 512
+
+  architectures = ["x86_64"]
 }
 
 variable "ecr_image_identifier" {
@@ -29,6 +81,6 @@ variable "ecr_image_identifier" {
   type        = string
 }
 
-output "service_url" {
-  value = aws_apprunner_service.staging.service_url
+output "lambda_function_name" {
+  value = aws_lambda_function.staging.function_name
 }
